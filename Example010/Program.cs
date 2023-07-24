74a91d1b9c1ad19a19eab1bda2eef842e34cfe2b
KirillBunakov <lonley987@gmail.com>
2023-07-24 16:39:19 +0300

    Добавил папки после лекции2

---
+++ Example010/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = {1,2,3,7,5,8,7,8};
+int n = array.Length;
+int find = 7;
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+
+  index++;
+}
+
+
+
+
+
